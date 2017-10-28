272ed063a8793fe554dd96818766a48152942aaa
marachi2 <marissaf32@gmail.com>
2017-10-27 19:28:04 -0600

    ran build a third time

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // import { key } from './config.js'
-require('dotenv').config
+require('dotenv').config();
 
 export const lookUp = function(search) {
     return axios.get('http://food2fork.com/api/search?key=' + process.env.APIKEY + '&q=' + search)
